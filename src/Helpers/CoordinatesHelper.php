2d357a011e197af150b344997cd8b87652484f7b
Matias Camiletti <matias.camiletti@gmail.com>
2023-08-08 19:09:25 -0300

    Add calculate distance in miles.

--- src/Helpers/CoordinatesHelper.php
+++ src/Helpers/CoordinatesHelper.php
@@ -43,4 +43,17 @@ class CoordinatesHelper
         // Devolver las nuevas coordenadas
         return ['latitude' => $nuevaLatitud, 'longitude' => $nuevaLongitud];
     }
+
+    public static function calculateDistanceInMiles($lat1, $lon1, $lat2, $lon2)
+    {
+        $earthRadius = 3959; // Radio de la Tierra en millas
+
+        $dLat = deg2rad($lat2 - $lat1);
+        $dLon = deg2rad($lon2 - $lon1);
+
+        $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        return $earthRadius * $c;
+    }
 }
